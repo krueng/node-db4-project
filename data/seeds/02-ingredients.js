77a6f21b4afc3b5e31d66a554893c0f100e7a83c
Adam K <krueng@gmail.com>
2021-11-04 22:39:03 -0500

    Completed all tasks

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,6 +1,6 @@
 exports.seed = function (knex, Promise) {
     return knex('ingredients').insert([
-        { ingredient_name: "salt"},
-        {ingredient_name: "olive oil", ingredient_unit: "lbs" }
+        {ingredient_name: "olive oil", ingredient_unit: "lbs" },
+        { ingredient_name: "salt"}
     ])
 }
